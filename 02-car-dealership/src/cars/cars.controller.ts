255ee51bd187f6ca1f2e5e78324070dff4ab6318
Francisco Timez <franciscotimez@gmail.com>
2023-04-03 14:22:51 -0300

    CRUD Controller

--- 02-car-dealership/src/cars/cars.controller.ts
+++ 02-car-dealership/src/cars/cars.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 // * Aca se declaran los endpoints
@@ -14,5 +23,20 @@ export class CarsController {
   @Get(':id')
   getCarById(@Param('id', ParseIntPipe) id: number) {
     return this.carsService.findOneById(+id);
+  }
+
+  @Post()
+  createCar(@Body() body: any) {
+    return { body };
+  }
+
+  @Patch(':id')
+  updateCar(@Param('id', ParseIntPipe) id: number, @Body() body: any) {
+    return { id, body };
+  }
+
+  @Delete(':id')
+  deleteCar(@Param('id', ParseIntPipe) id: number) {
+    return { id };
   }
 }

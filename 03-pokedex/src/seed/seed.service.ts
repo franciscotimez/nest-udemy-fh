61d64c0ee8084017177aa2d3fd4dd697350a42fa
Francisco Timez <franciscotimez@gmail.com>
2023-04-05 13:27:36 -0300

    http adapter with axios

--- 03-pokedex/src/seed/seed.service.ts
+++ 03-pokedex/src/seed/seed.service.ts
@@ -1,18 +1,16 @@
 import { Injectable } from '@nestjs/common';
-import { AxiosInstance } from 'axios';
-import axios from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
 import { InjectModel } from '@nestjs/mongoose';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { Model } from 'mongoose';
+import { AxiosAdapter } from '../common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
-  private readonly axios: AxiosInstance = axios;
-
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
   ) {}
 
   async executeSeed() {
@@ -20,12 +18,12 @@ export class SeedService {
     await this.pokemonModel.deleteMany();
 
     // console.log('Fetching data...');
-    const { data } = await this.axios.get<PokeResponse>(
+    const { results } = await this.http.get<PokeResponse>(
       `https://pokeapi.co/api/v2/pokemon?limit=650`,
     );
 
     // console.log('Loading dataBase');
-    const pokemonToInsert = data.results.map(({ name, url }) => {
+    const pokemonToInsert = results.map(({ name, url }) => {
       const segments = url.split('/');
       const no = +segments[segments.length - 2];
       return { name, no };

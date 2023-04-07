ac05c648809958ea7dc3e2506e34d320491cc1ec
Francisco Timez <franciscotimez@gmail.com>
2023-04-07 17:02:18 -0300

    Configurar conexion postgres en el proyecto

--- 04-teslo-shop/src/app.module.ts
+++ 04-teslo-shop/src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      database: process.env.DB_DATABASE,
+      host: process.env.DB_HOST,
+      password: process.env.DB_PASSWORD,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      autoLoadEntities: true,
+      synchronize: true, // ! Usualmente solo en desarrollo
+    }),
+  ],
   controllers: [],
   providers: [],
 })

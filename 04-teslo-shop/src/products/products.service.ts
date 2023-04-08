c48da060cd39a6fec700786988680e122ca20d93
Francisco Timez <franciscotimez@gmail.com>
2023-04-07 21:08:51 -0300

    find by term

--- 04-teslo-shop/src/products/products.service.ts
+++ 04-teslo-shop/src/products/products.service.ts
@@ -11,6 +11,7 @@ import { Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
 import { NotFoundException } from '@nestjs/common';
 import { PaginationDto } from '../common/dto/pagination.dto';
+import { validate as isUUID } from 'uuid';
 
 @Injectable()
 export class ProductsService {
@@ -42,11 +43,23 @@ export class ProductsService {
     });
   }
 
-  async findOne(id: string) {
-    const product = await this.productRepository.findOneBy({ id });
+  async findOne(term: string) {
+    let product: Product;
+
+    if (isUUID(term)) {
+      product = await this.productRepository.findOneBy({ id: term });
+    } else {
+      const queryBuilder = this.productRepository.createQueryBuilder();
+      product = await queryBuilder
+        .where('LOWER(title) =:title or slug =:slug', {
+          title: term.toLowerCase(),
+          slug: term.toLowerCase(),
+        })
+        .getOne();
+    }
 
     if (!product)
-      throw new NotFoundException(`Product with id '${id}' not found`);
+      throw new NotFoundException(`Product with term '${term}' not found`);
     return product;
   }
 

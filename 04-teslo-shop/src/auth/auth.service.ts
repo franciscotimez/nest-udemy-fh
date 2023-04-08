88bc4ffc8c73841646dc126776dd031c8d822638
Francisco Timez <franciscotimez@gmail.com>
2023-04-08 16:58:25 -0300

    Check auth status

--- 04-teslo-shop/src/auth/auth.service.ts
+++ 04-teslo-shop/src/auth/auth.service.ts
@@ -62,6 +62,13 @@ export class AuthService {
     };
   }
 
+  checkAuthStatus(user: User) {
+    return {
+      user,
+      token: this.getJwtToken({ id: user.id }),
+    };
+  }
+
   private getJwtToken(payload: IJwtPayload) {
     const token = this.jwtService.sign(payload);
     return token;

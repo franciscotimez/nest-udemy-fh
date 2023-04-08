c0e8a67d2268edcfdb0721f0df89a19d2774456c
Francisco Timez <franciscotimez@gmail.com>
2023-04-08 11:41:15 -0300

    serve public images

--- 04-teslo-shop/src/app.module.ts
+++ 04-teslo-shop/src/app.module.ts
@@ -1,6 +1,9 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
 import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
@@ -18,6 +21,9 @@ import { FilesModule } from './files/files.module';
       username: process.env.DB_USERNAME,
       autoLoadEntities: true,
       synchronize: true, // ! Usualmente solo en desarrollo
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
     }),
     ProductsModule,
     CommonModule,
--- 04-teslo-shop/src/main.ts
+++ 04-teslo-shop/src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe, Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
-async function bootstrap() {
+async function main() {
   const app = await NestFactory.create(AppModule);
-
+  const logger = new Logger('main');
   app.setGlobalPrefix('api');
 
   app.useGlobalPipes(
@@ -14,6 +14,7 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
+  logger.log(`App running on port ${process.env.PORT}`);
 }
-bootstrap();
+main();

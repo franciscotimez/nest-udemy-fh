cdd8e7821dee809a9d42e50e3b3e98e51d5852e8
Francisco Timez <franciscotimez@gmail.com>
2023-04-08 17:12:16 -0300

    add swager basic

--- 04-teslo-shop/src/main.ts
+++ 04-teslo-shop/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe, Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function main() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +14,15 @@ async function main() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Teslo API - FH')
+    .setDescription('Teslo API endpoints')
+    .setVersion('1.0')
+    .addTag('products')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(process.env.PORT);
   logger.log(`App running on port ${process.env.PORT}`);

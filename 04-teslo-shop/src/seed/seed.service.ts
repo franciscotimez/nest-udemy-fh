1aea25505865e4c779cdaace89a1708d950cfece
Francisco Timez <franciscotimez@gmail.com>
2023-04-08 09:37:39 -0300

    import and export Service

--- 04-teslo-shop/src/seed/seed.service.ts
+++ 04-teslo-shop/src/seed/seed.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { ProductsService } from '../products/products.service';
 
 @Injectable()
 export class SeedService {
+  constructor(private readonly productsService: ProductsService) {}
   async runSeed() {
-    return 'ruun seed';
+    await this.insertNewProducts();
+
+    return 'Seed Executed!';
+  }
+
+  private async insertNewProducts() {
+    await this.productsService.deleteAllProducts();
+    return true;
   }
 }

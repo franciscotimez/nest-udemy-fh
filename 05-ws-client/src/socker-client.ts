91f427e6e9d6a5dc401c76f4a3a1854e249fad8f
Francisco Timez <franciscotimez@gmail.com>
2023-04-10 10:59:03 -0300

    Client - Show status connection

--- 05-ws-client/src/socker-client.ts
+++ 05-ws-client/src/socker-client.ts
@@ -1,4 +1,5 @@
 import { Manager } from 'socket.io-client';
+import { Socket } from 'socket.io';
 
 export const connectToServer = () => {
 
@@ -6,6 +7,20 @@ export const connectToServer = () => {
   const manager = new Manager('http://localhost:3000/socket.io/socket.io.js');
 
   const socket = manager.socket('/');
-  console.log({ socket });
+  // console.log({ socket });
 
+  addListeners(socket);
+
+};
+
+const addListeners = (socket: Socket) => {
+  const serverStatusLabel = document.querySelector('#server-status')!;
+
+  socket.on('connect', () => {
+    serverStatusLabel.innerHTML = 'Online';
+  });
+
+  socket.on('disconnect', () => {
+    serverStatusLabel.innerHTML = 'Offline';
+  });
 };
